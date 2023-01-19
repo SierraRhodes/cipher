e97897dfd346b7469bd465dba34f443e5cb4ffd1
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-18 20:14:19 -0800

    Add first and last character finder.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//business logic
+function firstLast(letter1,letter2) {
+const first = sentence.charAt(0);
+const last = sentence.charAt(sentence.length - 1);
+}
+
+
+
+//user logic
+const sentence = prompt("Enter in a sentence: ");
+
+window.alert(sentence);
